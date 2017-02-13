730433bb6c63fbfcc47cc03ec1df647e4dd784dd
hellrich <hellrich.j@gmail.com>
2017-02-13 16:44:56 +0100

    rpbuster parsing

--- website/src/main/java/database/corpus/DTAMapper.java
+++ website/src/main/java/database/corpus/DTAMapper.java
@@ -3,7 +3,9 @@ package database.corpus;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
+import java.util.Arrays;
 import java.util.HashMap;
+import java.util.Iterator;
 import java.util.Map;
 
 public class DTAMapper implements WordMapper {
@@ -11,8 +13,16 @@ public class DTAMapper implements WordMapper {
 	private final Map<String, String> mapping = new HashMap<>();
 
 	public DTAMapper(Path path) throws IOException {
-		Files.lines(path).map(line -> line.split(";"))
-				.forEach(s -> mapping.put(s[0], s[1]));
+		Iterator<String[]> iter = Files.lines(path).map(line -> line.split(";"))
+				.iterator();
+		while (iter.hasNext()) {
+			String[] s = iter.next();
+			try {
+				mapping.put(s[0], s[1]);
+			} catch (ArrayIndexOutOfBoundsException e) {
+				System.err.println(Arrays.toString(s));
+			}
+		}
 	}
 
 	@Override
