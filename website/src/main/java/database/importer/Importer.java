f7c05369ae35ede26efbe0c55a4339d86c20df4a
hellrich <hellrich.j@gmail.com>
2017-02-27 10:45:58 +0100

    can handle gz

--- website/src/main/java/database/importer/Importer.java
+++ website/src/main/java/database/importer/Importer.java
@@ -1,9 +1,19 @@
 package database.importer;
 
+import java.io.BufferedInputStream;
+import java.io.BufferedReader;
+import java.io.Closeable;
+import java.io.File;
 import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.UncheckedIOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.util.Iterator;
+import java.util.stream.Stream;
+import java.util.zip.GZIPInputStream;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -11,22 +21,66 @@ import org.sql2o.Query;
 import org.sql2o.Sql2o;
 
 public abstract class Importer {
-	static final long IMPORT_BATCH_SIZE = 100000;
-	static final long IMPORT_COMMIT_SIZE = 10 * IMPORT_BATCH_SIZE;
+	static class GZIPFiles {
+		private static void closeSafely(final Closeable closeable) {
+			if (closeable != null)
+				try {
+					closeable.close();
+				} catch (final IOException e) {
+					// Ignore
+				}
+		}
 
+		/**
+		 * Get a lazily loaded stream of lines from a gzipped file, similar to
+		 * {@link Files#lines(java.nio.file.Path)}.
+		 *
+		 * From
+		 * https://erikwramner.wordpress.com/2014/05/02/lazily-read-lines-from-gzip-file-with-java-8-streams/
+		 *
+		 * @param path
+		 *            The path to the gzipped file.
+		 * @return stream with lines.
+		 */
+		public static Stream<String> lines(final Path path) {
+			InputStream fileIs = null;
+			BufferedInputStream bufferedIs = null;
+			GZIPInputStream gzipIs = null;
+			try {
+				fileIs = Files.newInputStream(path);
+				// Even though GZIPInputStream has a buffer it reads individual bytes
+				// when processing the header, better add a buffer in-between
+				bufferedIs = new BufferedInputStream(fileIs, 65535);
+				gzipIs = new GZIPInputStream(bufferedIs);
+			} catch (final IOException e) {
+				closeSafely(gzipIs);
+				closeSafely(bufferedIs);
+				closeSafely(fileIs);
+				throw new UncheckedIOException(e);
+			}
+			final BufferedReader reader = new BufferedReader(
+					new InputStreamReader(gzipIs));
+			return reader.lines().onClose(() -> closeSafely(reader));
+		}
+	}
+
+	static final long IMPORT_BATCH_SIZE = 100000;
+
+	static final long IMPORT_COMMIT_SIZE = 10 * IMPORT_BATCH_SIZE;
 	public static final String FREQUENCY_CSV = "FREQUENCY.csv";
 	public static final String PPMI_CSV = "PPMI.csv";
 	public static final String CHI_CSV = "CHI.csv";
 	public static final String SIMILARITY_CSV = "SIMILARITY.csv";
+
 	public static final String WORDS_CSV = "WORDIDS.csv";
 
 	private static final Logger LOGGER = LoggerFactory
 			.getLogger(Importer.class);
 
 	private final String sql;
-
 	private final String tableName;
 	private final Sql2o sql2o;
+
 	private final int corpusId;
 
 	protected Importer(final String sqlTemplate, final Sql2o sql2o,
@@ -39,9 +93,16 @@ public abstract class Importer {
 
 	abstract protected Query addParameters(final String[] s, final Query query);
 
-	public void importStuff(final Path path) throws IOException {
+	public void importStuff(Path path) throws IOException {
+		Stream<String> stream;
+		if (!path.toFile().exists()
+				&& new File(path.toFile().getPath() + ".gz").exists()) {
+			path = Paths.get(path.toFile().getPath() + ".gz");
+			stream = GZIPFiles.lines(path);
+		} else
+			stream = Files.lines(path);
 		LOGGER.info("Starting import of {} into {}", path, tableName);
-		final Iterator<String[]> iter = Files.lines(path).map(x -> x.split(","))
+		final Iterator<String[]> iter = stream.map(x -> x.split(","))
 				.iterator();
 		long i = 0;
 		Query query = null;
