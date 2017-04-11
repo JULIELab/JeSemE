2bbea1dd64a698abb89fb05a98c5b52a75b9a6ec
hellrich <johannes.hellrich@uni-jena.de>
2017-04-11 12:45:27 +0200

    option to download models

--- pipeline/train_models/make-downloads.sh
+++ pipeline/train_models/make-downloads.sh
@@ -1,9 +1,14 @@
 cd /home/hellrich/tmp/jedisem
 mkdir -p downloads
-rm downloads/*
+rm -f downloads/*
 
 for x in coha50 dta50 google_fiction google_german_lemmata royal_society_corpus/models
-	do zip -q downloads/$x.zip $x/*/{chi,pmi,svd_pmi}.* &
+do
+	(
+		name=${x%/models}
+		zip -q downloads/$name.zip $x/*/{chi,pmi,svd_pmi}.*
+		echo "finished $name"
+	)&
 done
 
 wait
