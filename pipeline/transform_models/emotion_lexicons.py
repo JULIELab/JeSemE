4b7aecaca10b488ab534ca8b11b0e62347e9611b
buechel <sven-buechel@gmx.de>
2017-10-11 10:15:09 +0200

    modified lexicon loading

--- pipeline/transform_models/emotion_lexicons.py
+++ pipeline/transform_models/emotion_lexicons.py
@@ -1,36 +1,35 @@
 import pandas as pd
 
-path_english=''
-path_german=''
 
-def load_english():
+### function that rescales data
+def scaleInRange(x, oldmin, oldmax, newmin,newmax):
+    #linear scaling (see koeper 2016). (Softmax makes no sense)
+    return ((newmax-newmin)*(x-oldmin))/(oldmax-oldmin)+newmin
+
+def load_english(path):
 	'''
 	Warriner et al. 2013
 	'''
-	if path_english=='':
-		raise ValueError('Path to the lexicon file is empty!'+\
-			'Please make sure to insert the correct file path')
-	warriner13 = pd.read_csv(path_english, sep=',')
+	warriner13 = pd.read_csv(path, sep=',')
 	warriner13=warriner13[['Word','V.Mean.Sum', 'A.Mean.Sum', 'D.Mean.Sum']]
-	warriner13.columns=heads_vad
+	warriner13.columns=['Word', 'Valence', 'Arousal', 'Dominance']
 	warriner13.set_index('Word',inplace=True)
 	# print(warriner13.head())
 	# print(warriner13.shape)
 	return warriner13
 
 
-def load_german():
+def load_german(path):
 	'''
 	Schmidtke et al., 2014
 	'''
-	if path_german=='':
-		raise ValueError('Path to the lexicon file is empty!'+\
-			'Please make sure to insert the correct file path.')
-	schmidtke14=pd.read_csv(path_german, sep='\t')
-	schmidtke14=schmidtke14[['Word','Valence','Arousal','Dominance']]
-	schmidtke14.columns=heads_vad
-	schmidtke14['Word']=schmidtke14['Word'].str.lower()
+	schmidtke14=pd.read_excel(path)
+	# schmidtke14=schmidtke14[['Word','Valence','Arousal','Dominance']]
+	schmidtke14=schmidtke14[['G-word', 'VAL_Mean', 'ARO_Mean_(ANEW)', 'DOM_Mean']]
+	schmidtke14.columns=['Word', 'Valence', 'Arousal', 'Dominance']
+	# schmidtke14['Word']=schmidtke14['Word'].str.lower()
 	schmidtke14.set_index('Word', inplace=True)
+	schmidtke14=schmidtke14[~schmidtke14.index.duplicated(keep='first')]
 	## rescaling valence
 
 	schmidtke14.Valence = [scaleInRange(x = x, oldmin = -3.,
