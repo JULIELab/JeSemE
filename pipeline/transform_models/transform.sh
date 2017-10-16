9d4bff4c34fbc90b1285659df800bf790f7f1668
hellrich <johannes.hellrich@uni-jena.de>
2017-10-16 14:13:25 +0200

    reordered arguments

--- pipeline/transform_models/transform.sh
+++ pipeline/transform_models/transform.sh
@@ -18,11 +18,11 @@ DE_EMO=$EMOPATH/13428_2013_426_MOESM1_ESM.xlsx
         echo "finished $x" ) &
 (       x="dta"
         mkdir -p $TARGET/$x
-        python vectors2similarity.py $TARGET/$x 10000 de $DE_EMO $SOURCE/dta50/{1751_1780,1781_1810,1811_1840,1841_1870,1871_1900} 
+        python vectors2similarity.py $TARGET/$x 10000 $DE_EMO de $SOURCE/dta50/{1751_1780,1781_1810,1811_1840,1841_1870,1871_1900} 
         echo "finished $x" ) &
 (       x="google_german"
         mkdir -p $TARGET/$x
-        python vectors2similarity.py $TARGET/$x 10000 de $DE_EMO $SOURCE/$x/{1830,1840,1850,1860,1870,1880,1890,1900,1910,1920,1930,1940,1950,1960,1970,1980,1990,2000} 
+        python vectors2similarity.py $TARGET/$x 10000 $DE_EMO de $SOURCE/$x/{1830,1840,1850,1860,1870,1880,1890,1900,1910,1920,1930,1940,1950,1960,1970,1980,1990,2000} 
         echo "finished $x" ) &
 wait
 echo "done"
