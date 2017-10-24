c46fbb36b0abe31d12b3a7ddb1433edadcedf937
hellrich <johannes.hellrich@uni-jena.de>
2017-10-24 16:11:33 +0200

    typo

--- pipeline/transform_models/scale_emotion.sh
+++ pipeline/transform_models/scale_emotion.sh
@@ -3,7 +3,7 @@ TARGET="/home/hellrich/tmp/jedisem/jeseme_v2_import"
 function scale {
         local target=$1
         python scale_emotion_data.py $target/EMOTION.csv
-        mv $target/EMOTION.csv mv $target/EMOTION_raw.csv
+        mv $target/EMOTION.csv $target/EMOTION_raw.csv #did not work in last run due to typo :/
         mv $target/EMOTION_normalized.csv $target/EMOTION.csv
 }
 
